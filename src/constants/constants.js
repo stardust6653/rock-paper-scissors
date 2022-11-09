97914c7839bdf8d9b9d64042821c48dccaaecd0f
stardust6653 <stardust6653@gmail.com>
2022-11-09 11:34:01 +0900

    Feat : Constants 추가
    - 이미지 URL 상수 선언

---
+++ src/constants/constants.js
@@ -0,0 +1,10 @@
+export const SCISSORS_IMG =
+  "https://thumb.silhouette-ac.com/t/a7/a7c3020b4cfb4fd154c4fcfd62702df2_w.jpeg";
+
+export const ROCK_IMG =
+  "https://thumb.silhouette-ac.com/t/69/69ffced83032519ca680dc3058b9ca26_w.jpeg";
+
+export const PAPER_IMG =
+  "https://thumb.silhouette-ac.com/t/26/2654adfd65b6ca4a8ac25a9f727d2262_w.jpeg";
+
+export const DEFAULT_IMG = "https://thumb.photo-ac.com/db/dbb9965f47e7a72ad982fce30b5322c1_w.jpeg";

385675a93ac951389ef0fa88324143a8767de79b
stardust6653 <stardust6653@gmail.com>
2022-11-09 15:56:22 +0900

    Style : Button CSS 변경가
    - hover시 border 2px에서 4px로 변경

--- src/components/BattleField/SelectButton.js
+++ src/components/BattleField/SelectButton.js
@@ -2,17 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const SelectButton = ({ img, onClick }) => {
-  const SelectButtonComponent = styled.div`
+  const SelectButtonComponent = styled.button`
     width: 5rem;
     height: 5rem;
     border: 2px solid black;
     transition: all 0.3s ease-in-out;
     margin-right: 2rem;
     &:hover {
-      border: 2px solid tomato;
+      border: 4px solid tomato;
       border-radius: 8%;
       cursor: pointer;
     }
+
     &:last-child {
       margin-right: 0;
     }

33a81ff417b88fad59e33f4a17f826241c44c9ea
stardust6653 <stardust6653@gmail.com>
2022-11-13 00:33:20 +0900

    Style : Alarm창 마크업
    - 저장 및 로드 시 팝업되는 알람창 마크업

---
+++ src/components/BattleField/Alarm.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+const PopUp = styled.div`
+  background-color: whitesmoke;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30rem;
+  height: 20rem;
+  border: 2px solid black;
+  font-size: 2.5rem;
+`;
+
+const ExitBtn = styled.button`
+  background-color: transparent;
+  position: absolute;
+  width: 2rem;
+  height: 2rem;
+  font-size: 1.5rem;
+  top: 1rem;
+  left: 1rem;
+  &:active {
+    background-color: #999;
+  }
+`;
+
+function Alarm({ comment, clickExitAlarmBtn }) {
+  return (
+    <PopUp>
+      <ExitBtn onClick={clickExitAlarmBtn}>X</ExitBtn>
+      <div>{comment}</div>
+    </PopUp>
+  );
+}
+
+export default Alarm;
